702f76ac1b64ba5401a99e6276898c3a05fa1dd9
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-08-06 17:43:21 +0300

    Создана библиотека для сущностей

---
+++ src/PS.PollingSystem/PS.PollingSystem.Entities/Answer.cs
@@ -0,0 +1,40 @@
+﻿using PS.PollingSystem.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.PollingSystem.Entities
+{
+    [DebuggerDisplay("{Title} {Votes} {Percents}")]
+    public class Answer : Identity
+    {
+
+
+        public string Title { get; init; }
+        public int Votes { get; set; }
+        public double Percents { get; set; }      
+
+        public Answer(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public void SetPercents(int totalVotes)
+        {
+            if(totalVotes > 0)
+            {
+                Percents = Votes * 100 / totalVotes;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Votes} ({Percents:F}%)";
+        }
+
+    }
+}
---
+++ src/PS.PollingSystem/PS.PollingSystem.Entities/Poll.cs
@@ -0,0 +1,44 @@
+﻿using PS.PollingSystem.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.PollingSystem.Entities
+{
+    [DebuggerDisplay("{QuestionText}")]
+    public  class Poll : Identity
+    {
+        public string QuestionText { get; set; } = null!;
+        public List<Answer>? Answers { get; set; }
+
+
+        public Poll(string questionText, List<Answer> answers) : this(questionText)
+        {
+            Answers = answers;
+        }
+
+        public Poll(string questionText)
+        {
+            QuestionText = questionText;
+        }
+
+
+        public void VoteTo(Guid id, int value = 1)
+        {
+            var item = Answers?.SingleOrDefault(x => x.Id == id);
+
+            if(item == null) { return; }
+
+            item.Votes += value;
+
+            var totalVotes = Answers?.Sum(x => x.Votes) ?? 0;
+
+            Answers?.ForEach(x => x.SetPercents(totalVotes));
+
+        }
+
+    }
+}
